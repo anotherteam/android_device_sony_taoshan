81e3a49b3134b7802174dfff2f6af17f5a1d1e8a
Abiel Zulio M <abielzuliom@users.noreply.github.com>
2015-02-19 11:11:54 +0700

    taoshan: rename default bluetooth local name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Xperia L"
+#define BTM_DEF_LOCAL_NAME   "Sony Xperia L"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #endif
